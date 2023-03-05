f61df04d931c92afadfb85d5f93c6e19c3089de3
Keitumetse <88365145+Zhanhu079@users.noreply.github.com>
2023-03-05 19:48:34 +0200

    Add project files.

---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim userName As String = "admin"
+        Dim password As String = "Tumi1085"
+
+        If (TextBox1.Text = userName And TextBox2.Text = password) Then
+            MessageBox.Show("Login succesful!")
+        Else
+            MessageBox.Show("Unknown User!")
+        End If
+    End Sub
+End Class
